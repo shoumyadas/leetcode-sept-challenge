4f8ff1f08773e29347ae5cdd25b1ea5a1ec46a18
Shoumya Kanti Das <shoumyakanti@gmail.com>
2020-09-03 02:36:52 +0530

    Update Day2-ContainsDuplicateIII.java

--- Day2-ContainsDuplicateIII.java
+++ Day2-ContainsDuplicateIII.java
@@ -1,19 +1,31 @@
+/**
+ * Given an array of integers, find out whether there are two distinct indices i
+ * and j in the array such that the absolute difference between nums[i] and
+ * nums[j] is at most t and the absolute difference between i and j is at most
+ * k.
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [1,2,3,1], k = 3, t = 0 Output: true Example 2:
+ * 
+ * Input: nums = [1,0,1,1], k = 1, t = 2 Output: true Example 3:
+ * 
+ * Input: nums = [1,5,9,1,5,9], k = 2, t = 3 Output: false
+ */
+
 class Solution {
-      public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-	boolean found = false;
-	for (int i = 0; i < nums.length; i++) {
+    public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+	int arrLen = nums.length;
+	for (int i = 0; i < arrLen; i++) {
 	    for (int x = 1; x <= k; x++) {
-		if ((i + x) < nums.length) {
-		    long diff = Math.abs(Long.valueOf(nums[i]) - Long.valueOf(nums[i + x]));
+		if ((i + x) < arrLen) {
+		    long diff = Math.abs(1L * nums[i] - nums[i + x]);
 		    if ((diff <= t)) {
-			found = true;
-			break;
+			return true;
 		    }
 		}
 	    }
-	    if (found)
-		break;
 	}
-	return found;
+	return false;
     }
 }
